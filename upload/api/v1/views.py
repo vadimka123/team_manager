3e0a1f9cd94fea620eb8b3b7aaaf972cf29937ca
vadimka123 <vadim.goy@smoozeedev.com>
2017-03-30 15:28:57 +0300

    fix for Line contains null byte

--- upload/api/v1/views.py
+++ upload/api/v1/views.py
@@ -20,7 +20,7 @@ class RecheckFileView(generics.GenericAPIView):
         file_delimiter = get_delimiter(request.data.get(u'file_delimiter', None), file)
         file_charset = get_file_encoding(file, request.data.get(u'file_charset', None))
 
-        headers = csv.reader(file, encoding=file_charset, delimiter=file_delimiter).next()
+        headers = csv.reader(file.replace(u'\0', u''), encoding=file_charset, delimiter=file_delimiter).next()
 
         return response.Response(data={
             u'headers': headers,
